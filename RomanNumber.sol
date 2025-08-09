be7d28b8165bceba5d87cbea10af0871dc17d897
synie8 <313524833@qq.com>
2025-08-09 10:20:56 +0800

    1

--- RomanNumber.sol
+++ RomanNumber.sol
@@ -6,6 +6,8 @@ contract RomanNumber{
            uint256 value ;
            string symbol ;
         }
+
+    mapping(bytes1 => int256) public romanMap;    
     
     // 正确声明动态数组
     Roman[] public romanNumbers;
@@ -24,6 +26,15 @@ contract RomanNumber{
         romanNumbers.push(Roman(5, "V"));
         romanNumbers.push(Roman(4, "IV"));
         romanNumbers.push(Roman(1, "I"));
+
+        romanMap['I'] = 1;
+        romanMap['V'] = 5;
+        romanMap['X'] = 10;
+        romanMap['L'] = 50;
+        romanMap['C'] = 100;
+        romanMap['D'] = 500;
+        romanMap['M'] = 1000;
+
     }
 
     function toRoman(uint256 num) public view  returns(string memory) {
@@ -40,4 +51,23 @@ contract RomanNumber{
 
         return res;
     }
+
+    function toInt(string memory s) public view  returns(int256 ) {
+        int256 rs=0;
+        bytes memory b = bytes(s);
+        uint n = b.length;
+
+        for(uint i=0;i<n;i++){
+            if(i<n-1 && romanMap[b[i]]<romanMap[b[i+1]]){
+                rs -= romanMap[b[i]];
+            }else{
+                rs += romanMap[b[i]];
+            }
+        }
+
+
+        return rs;
+
+    }
+
 }
